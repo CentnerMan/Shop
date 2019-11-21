02d5b557ead2ab1fa96b0d880beac8042ff99e17
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2019-11-21 11:35:15 +0300

    configs added

---
+++ shop-admin-ui/src/main/java/ru/vlsv/shopadminui/configs/ShopAdminUIConfig.java
@@ -0,0 +1,12 @@
+package ru.vlsv.shopadminui.configs;
+
+/**
+ * GeekBrains Java, Shop.
+ *
+ * @author Anatoly Lebedev
+ * @version 1.0.0 21.11.2019
+ * @link https://github.com/Centnerman
+ */
+
+public class AppConfig {
+}
---
+++ shop-ui/src/main/java/ru/vlsv/shopui/configs/ShopUIConfig.java
@@ -0,0 +1,12 @@
+package ru.vlsv.shopui.configs;
+
+/**
+ * GeekBrains Java, Shop.
+ *
+ * @author Anatoly Lebedev
+ * @version 1.0.0 21.11.2019
+ * @link https://github.com/Centnerman
+ */
+
+public class ShopUIConfig {
+}
