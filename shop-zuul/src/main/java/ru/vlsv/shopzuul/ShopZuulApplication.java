bc9d234e526297a2c0c0f16f248583f5df6583d5
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2019-12-09 01:42:21 +0300

    zuul + eureka test

---
+++ shop-zuul/src/main/java/ru/vlsv/shopzuul/ShopZuulApplication.java
@@ -0,0 +1,13 @@
+package ru.vlsv.shopzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopZuulApplication.class, args);
+    }
+
+}
